f7d77095613d908d28b0de90cae6ae62ecf23bcb
wombatwsei <7chocopl@gmail.com>
2019-03-31 14:48:59 +0200

    second lesson init

---
+++ 2.Wzorki/Wzorki/Example1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(" ");
+        static void SpaceLn() => Console.WriteLine(" ");
+        static void NewLine() => Console.WriteLine();
+        public static void Rectangle(int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+                Star();
+            NewLine();
+            for(int j = 1; j <= m-1; j++)
+            {
+                Star();
+                for (int i = 1; i < n - 1; i++)
+                    Space();
+                StarLn();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
