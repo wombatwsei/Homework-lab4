856e44faf57d4b5b90bd0ab52a40baec232a639a
wombatwsei <7chocopl@gmail.com>
2019-03-31 15:40:22 +0200

    2nd lesson 2nd commit

--- 2.Wzorki/Wzorki/Example1/Program.cs
+++ 2.Wzorki/Wzorki/Example1/Program.cs
@@ -26,11 +26,86 @@ namespace Example1
                     Space();
                 StarLn();
             }
+            for (int i = 0; i < n; i++)
+                Star();
+            NewLine();
+        }
+        static void letterX(int n)
+        {
+            if (n < 3) throw new ArgumentException("Size too small");
+            if (n % 2 == 0) n += 1;
 
+            for(int i=0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    //if (j == (n / 2) + 1)
+                    if (j == i || j == n - 1 - i)
+                    {
+                        Star();
+                    }
+                    else
+                    {
+                        Space();
+                    }
+                }
+                NewLine();
+            }
+        }
+        static void hourglass(int n)
+        {
+            for (int k = 0; k < n; k++) Star();
+            NewLine();
+            for (int i = 1; i < n-1; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //if (j == (n / 2) + 1)
+                    if (j == i || j == n - 1 - i)
+                    {
+                        Star();
+                    }
+                    else
+                    {
+                        Space();
+                    }
+                }
+                NewLine();
+            }
+            for (int l = 0; l < n; l++) Star();
+            NewLine();
+        }
+        static void chessboard(int n)
+        {
+            if (n % 2 == 1) n += 1;
+            int flag;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0) flag = 0;
+                else flag = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j % 2 == flag) Star();
+                    else Space();
+                }
+                NewLine();
+            }
         }
         static void Main(string[] args)
         {
-
+            Rectangle(10, 11);
+            NewLine();
+            letterX(11);
+            NewLine();
+            letterX(16);
+            NewLine();
+            hourglass(8);
+            NewLine();
+            hourglass(7);
+            NewLine();
+            chessboard(8);
+            NewLine();
+            chessboard(15);
         }
     }
 }
